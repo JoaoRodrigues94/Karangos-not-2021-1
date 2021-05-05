a82e350fef889fe1b70eebbbb99e80b231842866
JoaoRodrigues94 <joaopaulo.fr.1994@gmail.com>
2021-05-05 01:22:28 +0000

    (04/05/2021)

--- karangos/src/App.js
+++ karangos/src/App.js
@@ -24,9 +24,14 @@ import Button from '@material-ui/core/Button'
   </div> */
 
 import TopBar from './ui/TopBar';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles';
+import FooterBar from './ui/FooterBar';
+import { Box } from '@material-ui/core';
 import yellow from '@material-ui/core/colors/yellow';
 import pink from '@material-ui/core/colors/pink';
+import KarangosList from './routed/KarangosList';
+import KarangosForm from './routed/KarangosForm';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const theme = createMuiTheme({
   palette: {
@@ -40,10 +45,44 @@ const theme = createMuiTheme({
   },
 });
 
+const useStyles = makeStyles((theme) => ({
+  box: {
+    backgroundColor: theme.palette.background.default,
+    minHeight: '100vh'  // 100% da altura da área visível
+  },
+  routed: {
+    padding: '25px',
+    color: theme.palette.text.primary,
+    fontFamily: theme.typography.fontFamily
+  }
+}));
+
+function Main() {
+  const classes = useStyles();
+  return(
+    <Box className={classes.box}>
+      <BrowserRouter>
+        <TopBar  />
+        <Box id='routed' className={classes.routed}>
+          <Switch>
+            <Route path='/list'>
+              <KarangosList/>
+            </Route>
+            <Route path='/new'>
+              <KarangosForm />
+            </Route>
+          </Switch>
+        </Box>
+        <FooterBar />
+      </BrowserRouter>
+    </Box>
+  )
+}
+
 function App() {
   return (
     <ThemeProvider theme={theme} >
-        <TopBar  />
+      <Main />
     </ThemeProvider>
   );
 }
